69f1875254b522df810c8f7f0b3ef8da39a88b39
Joose Helle <joose.helle@gmail.com>
2020-02-08 17:34:11 +0200

    Implement AQI archiver

--- src/app/env_vars.py
+++ src/app/env_vars.py
@@ -1,12 +1,13 @@
 import os
 import csv
+from app.logger import Logger
 
-def set_env_vars():
+def set_env_vars(log: Logger):
     try:
         with open('.env', newline='') as csvfile:
             reader = csv.DictReader(csvfile, fieldnames=['key', 'value'], delimiter='=')
             for row in reader:
                 os.environ[row['key']] = row['value']
     except Exception:
-        print('no .env file found')
+        log.warning('no .env file found')
         pass
--- src/aqi_archiver_app.py
+++ src/aqi_archiver_app.py
@@ -1,30 +1,38 @@
 import time
 import traceback
-from datetime import datetime
-from app.aqi_archiver import AqiArchiver
 from app.logger import Logger
+from app.env_vars import set_env_vars
+from app.aqi_fetcher import AqiFetcher
+from app.aqi_uploader import AqiUploader
 
 log = Logger(b_printing=True, log_file='aqi_archiver.log')
-AQI = AqiArchiver(log, set_aws_secrets=True)
+set_env_vars(log)
+fetcher = AqiFetcher(log)
+uploader = AqiUploader(log)
 
-def process_new_aqi_data():
-    expected_aqi_zip_name = AQI.get_expected_aqi_download_name()
-    AQI.set_wip_aqi_download_name(expected_aqi_zip_name)
+def download_new_aqi_data():
+    expected_aqi_zip_name = fetcher.get_expected_aqi_download_name()
     try:
-        log.info('fetching Enfuser data...')
-        aqi_zip_name = AQI.fetch_enfuser_data(expected_aqi_zip_name)
-        log.info('downloaded aqi_zip: '+ aqi_zip_name)
+        fetcher.fetch_enfuser_data(expected_aqi_zip_name)
     except Exception:
-        log.error('failed to download AQI data: '+ AQI.wip_aqi_download)
+        log.error('failed to fetch AQI data: '+ expected_aqi_zip_name)
         traceback.print_exc()
-        time.sleep(45)
-    finally:
-        AQI.remove_old_aqi_files()
-        AQI.reset_wip_aqi_download_name()
+        time.sleep(60)
+
+def upload_new_aqi_data():
+    try:
+        uploader.upload_file_to_allas(fetcher.latest_aqi_download)
+    except Exception:
+        log.error('failed to upload AQI data to Allas: '+ fetcher.latest_aqi_download)
+        traceback.print_exc()
+        time.sleep(60)
+    uploader.remove_old_aqi_files()
 
 log.info('starting AQI archiving')
-
 while True:
-    if (AQI.new_aqi_download_available() == True):
-        process_new_aqi_data()
+    if (fetcher.new_aqi_download_available() == True):
+        download_new_aqi_data()
+    if (uploader.latest_aqi_upload != fetcher.latest_aqi_download):
+        upload_new_aqi_data()
+
     time.sleep(10)
